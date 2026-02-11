1ded90a0bd769f65f01bf33913f2e4c8fd300e89
AlanBenjaDev <alenciopalencio@gmail.com>
2026-02-11 17:02:42 -0300

    replace query

--- backend/dist/services/package.service.js
+++ backend/dist/services/package.service.js
@@ -6,23 +6,31 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.dashboardService = void 0;
 const db_1 = __importDefault(require("../config/db"));
 const dashboardService = async () => {
-    const [rows] = await db_1.default.query(`SELECT 
+    const [rows] = await db_1.default.query(`SELECT
     e.id AS envio_id,
     e.tipo_envio,
     e.direccion,
     e.ciudad,
     e.codigo_postal,
+    e.tracking_codigo,
     e.estado AS envio_estado,
     e.created_at AS fecha_envio,
+
     ped.id AS pedido_id,
-    prod.nombre AS producto_nombre,
-    prod.precio AS producto_precio,
     ped.total AS total_pedido,
-    ped.estado AS estado_pedido
-  FROM envios e
-  JOIN pedidos ped ON ped.id = e.pedido_id
-  JOIN productos prod ON prod.id = ped.producto_id
-  ORDER BY e.created_at DESC;
+    ped.estado AS estado_pedido,
+
+    pd.producto_id,
+    pd.cantidad,
+    pd.precio_unitario,
+    prod.producto AS nombre_producto
+
+FROM envios e
+JOIN pedidos ped ON ped.id = e.pedido_id
+JOIN pedido_detalle pd ON pd.pedido_id = ped.id
+JOIN productos prod ON prod.id = pd.producto_id
+ORDER BY e.created_at DESC;
+
   `);
     return rows;
 };
